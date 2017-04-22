4ba04345ce5c96dc6651dbfc80121030855b9bd7
Koki.Ide <s15087ki@sfc.keio.ac.jp>
2017-04-22 17:59:01 +0900

    Error　あり

--- PhotoMaster/ViewController.swift
+++ PhotoMaster/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    @IBOutlet var photoImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,43 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //camera button method
+    @IBAction func onTappedCameraButton() {
+        presentPickerController(sourceType: .camera)
+        
+    }
+    
+    @IBAction func onTappedAlbumButton() {
+        presentPickerController(sourceType: .photoLibrary)
+        
+    }
+    
+    func presentPickerController(sourceType: UIImagePickerControllerSourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]){
+        self.dismiss(animated: true, completion: nil)
+        
+        //画像出力
+        photoImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+    }
+    
+    func drawText(image: UIImage) -> UIImage {
+    
+    let text = "LifeisTech!/niPhoneアプリ開発コース"
+    
+    let textFontAttributes = [
+    NSFontAttributeName: UIFont(name: "Arial", size: 120)!,
+    NSForegroundColorAttributeName: UIColor.red]
+    }
+    
 
 }
 
